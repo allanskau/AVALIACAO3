5842efd5e4694396f46e55e70b933235a92ccf0d
allan <allan@ALLANSK>
2015-07-07 12:13:16 -0300

    primeiro commit

--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/LancamentoComparator.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/LancamentoComparator.java
@@ -6,5 +6,59 @@ package utfpr.ct.dainf.if62c.avaliacao;
  * @author 
  */
 public class LancamentoComparator {
+    private int ano;
+    private int mes;
+    private int dia;
+    private int conta;
+
+    public LancamentoComparator() {
+    }
+
+    public LancamentoComparator(int ano, int mes, int dia, int conta) {
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.conta = conta;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
     
+    private int comparaConta(int c1, int c2) {
+        if(c1 > c2){
+            return (c1 - c2);
+        }else if(c1 < c2){
+            return -(c1 - c2);
+        }else
+            return 0;
+    }
 }
--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/ProcessaLancamentos.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/ProcessaLancamentos.java
@@ -3,39 +3,87 @@ package utfpr.ct.dainf.if62c.avaliacao;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import static java.lang.System.exit;
+import java.sql.Date;
 import java.util.List;
 
 /**
- * IF62C Fundamentos de Programação 2
- * Avaliação parcial.
- * @author 
+ * IF62C Fundamentos de Programação 2 Avaliação parcial.
+ *
+ * @author
  */
 public class ProcessaLancamentos {
+
     private BufferedReader reader;
 
+    public ProcessaLancamentos() {
+    }
+/*
+    public ProcessaLancamentos(File f) {
+        if (f == null) {
+            System.out.println("Arquivo invalido");
+            exit(0);
+        }
+        try{
+            reader = new BufferedReader(new FileReader(f));
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Arquivo não encontrado: ");
+        } 
+        
+    }
+
+    public ProcessaLancamentos(String s) {
+        if (s == null) {
+            System.out.println("Arquivo invalido");
+            exit(0);
+        }
+        try {
+            reader = new BufferedReader(new FileReader(new File(s)));
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Arquivo não encontrado: ");
+        } 
+    }
+*/
     public ProcessaLancamentos(File arquivo) throws FileNotFoundException {
+        reader = new BufferedReader(new FileReader(arquivo));
+        throw new UnsupportedOperationException("Não implementado");
+    }
+    
+    public ProcessaLancamentos(String path) throws FileNotFoundException {
+        reader = new BufferedReader(new FileReader(new File(path)));
         throw new UnsupportedOperationException("Não implementado");
     }
 
-    public ProcessaLancamentos(String path) throws FileNotFoundException {
-        throw new UnsupportedOperationException("Não implementado");
-    }
-    
     private String getNextLine() throws IOException {
-        throw new UnsupportedOperationException("Não implementado");
+        try{
+            return reader.readLine();
+        }catch(IOException ioe){
+            throw new UnsupportedOperationException("Não implementado");
+        }
     }
-    
+
     private Lancamento processaLinha(String linha) {
-        throw new UnsupportedOperationException("Não implementado");
+        Integer conta;
+        Date data;
+        String descricao;
+        Double valor;
+        conta = Integer.valueOf(linha.substring(0, 7));
+        data = Date.valueOf(linha.substring(7, 15));
+        descricao = linha.substring(15, 76);
+        valor = Double.valueOf(linha.substring(76, 87));
+        return (new Lancamento(conta, data, descricao, valor));
     }
-    
+
     private Lancamento getNextLancamento() throws IOException {
+        
         throw new UnsupportedOperationException("Não implementado");
     }
-    
+
     public List<Lancamento> getLancamentos() throws IOException {
+        
         throw new UnsupportedOperationException("Não implementado");
     }
-    
+
 }
